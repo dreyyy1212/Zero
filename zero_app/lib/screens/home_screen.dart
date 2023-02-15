7a525135df37ec7f55518b85339dd8681195c184
Viet <avi261199@gmail.com>
2023-02-16 00:50:53 +0700

    fixing bug open camera

--- zero_app/lib/screens/home_screen.dart
+++ zero_app/lib/screens/home_screen.dart
@@ -179,8 +179,11 @@ class _HomePageState extends State<HomePage> {
     final data = await provider.scanQR();
     if (data == null) return;
     final userData = jsonDecode(data);
-    final user =
-        User(userData['accid'], userData['employeeCode'], userData['name']);
-    await provider.changeUser(user);
+    try {
+      final user =
+          User(userData['accid'], userData['employeeCode'], userData['name']);
+    } on Exception catch (e) {
+    }
+    // await provider.changeUser(user);
   }
 }
